2d5ae3484c7162b01bd692166c5988f6d1ebf868
olgamartinchik <sintcerova@mail.ru>
2020-10-16 10:48:16 +0300

    faet: add solutin of task Braskets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+    let simmetricalBrackets = [];
+    let openBrackets = [];
+    let closeBrackets = [];
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        let open = bracketsConfig[i][0];
+        let close = bracketsConfig[i][1];
+        if (open === close) {
+            simmetricalBrackets.push(open);
+        } else {
+            openBrackets.push(open);
+            closeBrackets.push(close);
+        }
+    }
+    for (let bracket of str) {
+        if (simmetricalBrackets.includes(bracket)) {
+            if (stack[stack.length - 1] === bracket) {
+                stack.pop()
+            } else {
+                stack.push(bracket)
+            }
+        } else if (openBrackets.includes(bracket)) {
+            stack.push(bracket)
+        } else if (closeBrackets.includes(bracket)) {
+            let pair = openBrackets[closeBrackets.indexOf(bracket)];
+            if (stack[stack.length - 1] === pair) {
+                stack.pop()
+            } else {
+                stack.push(bracket);
+            }
+        }
+    }
+
+    return (stack.length === 0)
+
 }
